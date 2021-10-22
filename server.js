34c33de7bbc334583db04e6f70db6a5067ee6839
Derick Hooley <dghooley@gmail.com>
2021-10-22 00:38:58 -0500

    insomnia

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const db = mysql.createConnection(
         // Your MySQL username,
         user: 'root',
         // Your MySQL password
-        password: '1Winter211980!',
+        password: '1Winter211980!2',
         database: 'election'
     },
     console.log('Connected to the election database.')
